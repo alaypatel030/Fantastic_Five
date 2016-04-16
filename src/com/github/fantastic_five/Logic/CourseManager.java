546ae08746ecf071e1b08416e0d6de598390601d
oitsjustjose <stovallj1995@gmail.com>
2016-04-16 16:23:23 -0400

    Fixes and better GUIs

--- src/com/github/fantastic_five/Logic/CourseManager.java
+++ src/com/github/fantastic_five/Logic/CourseManager.java
@@ -1,9 +1,5 @@
 package com.github.fantastic_five.Logic;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.Comparator;
 import java.util.HashSet;
